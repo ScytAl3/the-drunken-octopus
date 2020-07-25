eb4502df94d63a579b0326ed5f2bc9a1ca4f7573
Franck J <45272552+ScytAl3@users.noreply.github.com>
2020-07-25 21:49:01 +0200

    Add UserCrudController to EasyAdmin and and configure the menu items

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,18 +2,21 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\User;
+use App\Entity\Style;
 use App\Entity\Bottle;
 use App\Entity\Brewery;
 use App\Entity\Country;
 use App\Entity\Product;
-use App\Entity\Style;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
-use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
-use EasyCorp\Bundle\EasyAdminBundle\Router\CrudUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\UserMenu;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use Symfony\Component\Security\Core\User\UserInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Router\CrudUrlGenerator;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 
 /**
  * @IsGranted("ROLE_ADMIN_PRODUCT")
@@ -41,10 +44,26 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
+
+        // Section relative à la gestion des produits
+        yield MenuItem::section('Products management');
         yield MenuItem::linkToCrud('Product', 'fa fa-beer', Product::class);
-        yield MenuItem::linkToCrud('Style', 'fa fa-glass', Style::class);
-        yield MenuItem::linkToCrud('Country', 'fa fa-flag', Country::class);
-        yield MenuItem::linkToCrud('Brewery', 'fa fa-industry', Brewery::class);
-        yield MenuItem::linkToCrud('Bottle', 'fa fa-flask', Bottle::class);
+
+        // Sous menu relatif à la gestion des "catégories"
+        yield MenuItem::subMenu('Categories', 'fa fa-tags')->setSubItems([
+            MenuItem::linkToCrud('Style', 'fa fa-glass', Style::class),
+            MenuItem::linkToCrud('Country', 'fa fa-flag', Country::class),
+            MenuItem::linkToCrud('Brewery', 'fa fa-industry', Brewery::class),
+            MenuItem::linkToCrud('Bottle', 'fa fa-flask', Bottle::class)
+        ]);
+        
+        // Section relative à la gestion des utilisateurs
+        yield MenuItem::section('Users management');
+        yield MenuItem::linkToCrud('User', 'fa fa-user', User::class);
+    }
+
+    public function configureUserMenu(UserInterface $user): UserMenu
+    {
+        return parent::configureUserMenu($user);
     }
 }
