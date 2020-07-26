03be8f84836fb29a22e4a7750afd86a2a881ea88
Franck J <45272552+ScytAl3@users.noreply.github.com>
2020-07-26 15:13:45 +0200

    Add VichUploaderBundle and edit config

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
 ];
--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -9,10 +9,14 @@ use App\Repository\ProductRepository;
 use Symfony\Component\Validator\Constraints as Assert;
 // Validates that a particular field (or fields) in a Doctrine entity is (are) unique
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+// Link the upload mapping to Product entity
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
  * @ORM\Entity(repositoryClass=ProductRepository::class)
  * @ORM\Table(name="products")
+ * @Vich\Uploadable
  * @ORM\HasLifecycleCallbacks()
  * @UniqueEntity("title")
  */
@@ -122,6 +126,15 @@ class Product
      * @ORM\ManyToOne(targetEntity=Bottle::class, inversedBy="products")
      */
     private $bottle;
+
+    /**
+     * NOTE: This is not a mapped field of entity metadata, just a simple property.
+     * 
+     * @Vich\UploadableField(mapping="product_image", fileNameProperty="imageName")
+     * 
+     * @var File|null
+     */
+    private $imageFile;
 
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
@@ -294,6 +307,31 @@ class Product
         $this->bottle = $bottle;
 
         return $this;
+    }
+
+    /**
+     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+     * of 'UploadedFile' is injected into this setter to trigger the update. If this
+     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+     * must be able to accept an instance of 'File' as the bundle will inject one here
+     * during Doctrine hydration.
+     *
+     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile|null $imageFile
+     */
+    public function setImageFile(?File $imageFile = null): void
+    {
+        $this->imageFile = $imageFile;
+
+        if (null !== $imageFile) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->setUpdatedAt(new \DateTimeImmutable);
+        }
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
     }
 
     public function getImageName(): ?string
