42ae823983658b7c794498f91021c8552c4d96f7
Franck J <45272552+ScytAl3@users.noreply.github.com>
2020-07-27 22:21:56 +0200

    Copy files from assets/images via Encore and style products page

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,21 @@ Encore
     // only needed for CDN's or sub-directory deploy
     //.setManifestKeyPrefix('build/')
 
+    // copyFiles() method to copy images files into the final output directory
+    // see node_modules/@symfony/webpack-encore/index.js
+    .copyFiles({
+        from: './assets/images',
+        // optional target path, relative to the output dir
+        //to: 'images/[path][name].[ext]',
+
+        // if versioning is enabled, add the file hash too
+        to: '[path][name].[hash:8].[ext]',
+        context: './assets/'
+
+        // only copy files matching this pattern
+        //pattern: /\.(png|jpg|jpeg)$/
+    })
+
     /*
      * ENTRY CONFIG
      *
@@ -69,6 +84,6 @@ Encore
     // uncomment if you use API Platform Admin (composer req api-admin)
     //.enableReactPreset()
     //.addEntry('admin', './assets/js/admin.js')
-;
+    ;
 
 module.exports = Encore.getWebpackConfig();
