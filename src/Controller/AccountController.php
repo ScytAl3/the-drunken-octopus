1f7d03f14078e4a97833d32108435ed2af020cbe
Franck J <45272552+ScytAl3@users.noreply.github.com>
2020-08-28 19:20:07 +0200

    Update account controller, add sub account menu page with templates and update translation catalog

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -12,12 +12,34 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class AccountController extends AbstractController
 {
     /**
-     * @Route("/account", name="app_user_account")
+     * @Route("/account", name="app_account_index", methods={"GET"})
      */
     public function index()
     {
-        return $this->render('account/index.html.twig', [
-            
-        ]);
+        return $this->render('account/index.html.twig', []);
+    }
+
+    /**
+     * @Route("/account/identity", name="app_account_identity", methods={"GET"})
+     */
+    public function identity()
+    {
+        return $this->render('account/identity.html.twig', []);
+    }
+
+    /**
+     * @Route("/account/address", name="app_account_address", methods={"GET"})
+     */
+    public function address()
+    {
+        return $this->render('account/address.html.twig', []);
+    }
+
+    /**
+     * @Route("/account/order-history", name="app_account_order_history", methods={"GET"})
+     */
+    public function orderHistory()
+    {
+        return $this->render('account/order_history.html.twig', []);
     }
 }
